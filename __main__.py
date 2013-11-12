9f15844a5281ec790acf475651fd815e2a3d8178
Sam Gammon <sam@keen.io>
2013-11-11 22:33:05 -0800

    Adding local Protobuf and removing dist version from requirements

--- __main__.py
+++ __main__.py
@@ -18,10 +18,10 @@
 '''
 
 # stdlib
-import sys
+import sys, os
 
-sys.path.insert(0, 'coolapp')
-sys.path.insert(0, 'coolapp/lib')
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'coolapp'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'coolapp/lib'))
 
 ## apptools!
 from apptools import dispatch
